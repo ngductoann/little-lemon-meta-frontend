212383454f22fc0818b685a62cca86ef34227310
ngductoan0712 <ngductoan0712@gmail.com>
2025-04-01 09:42:07 +0700

    first commit

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import OurStory from "./OurStory";
+import Testimonials from "./Testimonials";
+import WeekSpecials from "./WeekSpecials";
+
+export const Home = () => {
+	return (
+		<>
+			<Hero />
+			<WeekSpecials />
+			<Testimonials />
+			<OurStory />
+		</>
+	);
+};
---
+++ src/components/pages/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import "./NotFound.css";
+
+const NotFound = () => {
+	return (
+		<div className="container page-not-found">
+			<h1>404</h1>
+			<h2>Page not found</h2>
+			<h2>Coming Soon</h2>
+		</div>
+	);
+};
+
+export default NotFound;
